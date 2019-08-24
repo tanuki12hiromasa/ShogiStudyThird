b63af188d794788a6eb224a101fc763b61fbf324
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2019-08-25 02:18:06 +0900

    apery
    SSSから移植

--- ShogiStudyThird/apery_evaluate.h
+++ ShogiStudyThird/apery_evaluate.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "apery_feature.h"
+#include "evaluated_nodes.h"
+
+namespace apery {
+	class apery_evaluator {
+	public:
+		static void init();
+		static void evaluate(EvaluatedNodes_full& en);
+		static void evaluate(EvaluatedNodes_diff& en, const apery_feat& pfeat);
+	};
+}
