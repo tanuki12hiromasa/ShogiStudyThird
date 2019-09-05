efe3464259b3fc9a519d62501174756e4a03c732
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2019-09-05 15:54:13 +0900

    agent backup

--- ShogiStudyThird/agent.cpp
+++ ShogiStudyThird/agent.cpp
@@ -31,26 +31,70 @@ void SearchAgent::simulate() {
 	//下り
 	yarinaoushi:
 	while (!node->isLeaf()) {
-		double evalmin = std::numeric_limits<double>::max();
+		//ノード選択
+		double CE = std::numeric_limits<double>::max();
+		std::vector<double> evals;
+		{
+			s_lock(node->_mutex);
+			for (const auto& child : node->children) {
+				double eval = child->getEvaluation();
+				evals.push_back(eval);
+				if (eval < CE)
+					CE = eval;
+			}
+		}
+		double Z = 0;
+		for (const auto& eval : evals) {
+			Z += std::exp(-(eval - CE) / T_c);
+		}
+		double pip = Z * random(engine);
+		const auto& children = node->children;
+		node = children.front();
+		auto child = children.begin();
+		for (const auto& eval : evals) {
+			pip -= std::exp(-(eval - CE) / T_c);
+			if (pip <= 0) {
+				node = *child;
+				break;
+			}
+			child++;
+		}
+		//局面を進める
+
+	}
+	//展開
+
+	{
+		/*
+			while (!node->isLeaf()) {
+		double CE = std::numeric_limits<double>::max();
 		std::vector<ChildN> evals;
 		{
 			s_lock(node->_mutex);
 			for (const auto& child : node->children) {
 				double eval = child->getEvaluation();
 				evals.push_back(ChildN(eval, child));
-				if (eval < evalmin)evalmin = eval;
+				if (eval < CE)
+					CE = eval;
 			}
 		}
 		double Z = 0;
-		for (const auto cn : evals) {
-			Z += std::exp(-(cn.first - evalmin) / T_c);
+		for (const auto& cn : evals) {
+			Z += std::exp(-(cn.first - CE) / T_c);
+		}
+		double pip = Z * random(engine);
+		node = evals.front().second;
+		for (const auto& cn : evals) {
+			pip -= std::exp(-(cn.first - CE) / T_c);
+			if (pip <= 0) {
+				node = cn.second;
+				break;
+			}
 		}
 
-
-
 	}
-	//展開
-
+		*/
+	}
 	//
 	//
 	//
