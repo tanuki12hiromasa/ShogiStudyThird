ccdadfad387642bb78b591324d357d4bf717b69a
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2019-10-01 04:37:23 +0900

    add player.constructor

--- ShogiStudyThird/player.h
+++ ShogiStudyThird/player.h
@@ -3,12 +3,14 @@
 
 class SearchPlayer {
 public:
+	Kyokumen kyokumen;
+	Feature feature;
+
+	SearchPlayer():kyokumen() { feature.set(kyokumen); }
+	SearchPlayer(const Kyokumen& _kyokumen) :kyokumen(_kyokumen) { feature.set(kyokumen); }
 	SearchPlayer(Kyokumen&& k,Feature&& f):kyokumen(k),feature(f){}
 	void proceed(Move m) {
 		feature.proceed(kyokumen,m);
 		kyokumen.proceed(m);
 	}
-
-	Kyokumen kyokumen;
-	Feature feature;
 };
