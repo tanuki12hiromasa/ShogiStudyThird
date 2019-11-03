918045725db4ea952a91f06f48516931f2a6f6be
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2019-11-04 02:45:58 +0900

    fix Search at Terminal
    子ノードのないノードを探索した時の不具合を修正

--- ShogiStudyThird/node.h
+++ ShogiStudyThird/node.h
@@ -49,7 +49,7 @@ public:
 	bool isLimitedExpanded()const { return state == State::QE || state == State::QT; }
 	bool isQSTerminal()const { return state != State::N && state != State::QE; }
 	bool isLeaf()const { return state == State::N || state == State::QE || state == State::QT; }
-	bool isTerminal()const { return state == State::T; }
+	bool isSearchable()const { return state != State::T; }
 
 	std::vector<SearchNode*> children;
 	Move move;
