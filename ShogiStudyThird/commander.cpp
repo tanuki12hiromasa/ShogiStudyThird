27321bec4d09c54e4f67684c1650afd0ea8a692c
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2020-03-11 03:04:15 +0900

    getBestChild関数に新パターンを追加
    深さ指標が(親ノードの深さ指標-1)の何割かに達していない場合は選外とする方法

--- ShogiStudyThird/commander.cpp
+++ ShogiStudyThird/commander.cpp
@@ -121,7 +121,7 @@ void Commander::coutOption() {
 	cout << "option name Ec_functionCode type spin default 18 min 0 max 19" << endl;
 	cout << "option name Ec_c type string default 0.5" << endl;
 	cout << "option name NodeMaxNum type string default 100000000" << endl;
-	cout << "option name PV_functionCode type spin default 0 min 0 max 2" << endl;
+	cout << "option name PV_functionCode type spin default 0 min 0 max 3" << endl;
 	cout << "option name PV_const type string default 0" << endl;
 }
 
