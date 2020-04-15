193da469bc5623270a15a0b2c41a5a8b6195aea9
kumesan <35446778+Kumekawa@users.noreply.github.com>
2020-04-15 17:41:00 +0900

    根ノードの指し手をnullmoveにするように変更

--- ShogiStudyThird/commander.cpp
+++ ShogiStudyThird/commander.cpp
@@ -507,7 +507,7 @@ void Commander::yomikomi()
 		}
 
 		if (index == 0) {//1つ目は親なし
-			test.push_back(node->restoreNode(move, st, eval, mass));
+			test.push_back(node->restoreNode(Move(), st, eval, mass));
 		}
 		else {
 			test.push_back(node->restoreNode(move, st, eval, mass));
